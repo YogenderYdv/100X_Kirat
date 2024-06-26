1aa8951fb23b3502cb2abeea66fb9c633cb18e74
YogenderYdv <yadavyogender2603@gmail.com>
2024-06-26 17:01:18 +0530

    Add files via upload

---
+++ week-3/3.1(midd,zod.global catch)/middleware.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const zod = require('zod');
+
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(5);
+
+function middleware(req, res, next) {
+    const {username,pass}=req.headers;
+    const email = emailSchema.safeParse(username);
+    const password = passwordSchema.safeParse(pass);
+
+    if (!email.success || email.data !== 'abc@gmail.com' || !password.success || password.data !== '12345') {
+        res.status(400).json({
+            msg: 'invalid'
+        });
+    } else {
+        next();
+    }
+}
+
+app.get('/', middleware, (req, res) => {
+    res.json({
+        msg: 'valid'
+    });
+});
+
+// Global error handler
+app.use(function(err, req, res, next) {
+    res.status(500).json({
+        msg: 'Upr kahi GALTI he !!'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+});
